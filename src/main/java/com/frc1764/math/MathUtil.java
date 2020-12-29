3ecff31ffc551c79ea4322a9257eab0ca4868afa
TheGreatChaosCoder <35439715+TheGreatChaosCoder@users.noreply.github.com>
2020-12-28 22:38:38 -0600

    added two new deadband functions

--- src/main/java/com/frc1764/math/MathUtil.java
+++ src/main/java/com/frc1764/math/MathUtil.java
@@ -13,6 +13,20 @@ public class MathUtil {
             return num;
         }
     }
+    
+    public static double deadbandLower(double num, double lowerLimit){
+        if(num>lowerLimit){
+            return 0;
+        }
+        return num;
+    }
+    
+    public static double deadbandUpper(double num, double upperLimit){
+        if(num<upperLimit){
+            return 0;
+        }
+        return num;
+    }
 
     public static double deadband(double num, double upperLimit, double lowerLimit){
         if(num<upperLimit && num>lowerLimit){
